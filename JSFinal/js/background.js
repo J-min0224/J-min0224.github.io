9018235f4156a7c4883e7d9707f42b6e2493b5e4
Jaemin Jeon <42255853+J-min0224@users.noreply.github.com>
2022-12-04 22:42:46 +0900

    Delete JSFinal directory

--- JSFinal/js/background.js
+++
@@ -1,8 +0,0 @@
-const imgs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"];
-
-const img = imgs[Math.floor((Math.random()) * imgs.length)];
-
-const bgImg = document.createElement("img");
-bgImg.src = `background/${img}`;
-
-document.body.prepend(bgImg);
